1302346610c0ee30c0a74310e772a3080fe6afb8
sleyzerzon <senya22@yahoo.com>
2016-09-12 19:40:19 -0400

    summator

--- akka.parallel-summator.java/src/main/java/com/excelsiorsoft/akka/parallel/summator/java/Summator.java
+++ akka.parallel-summator.java/src/main/java/com/excelsiorsoft/akka/parallel/summator/java/Summator.java
@@ -1,15 +1,72 @@
 package com.excelsiorsoft.akka.parallel.summator.java;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import akka.actor.ActorRef;
+import akka.actor.PoisonPill;
+import akka.actor.Props;
 import akka.actor.UntypedActor;
 
-public class Summator extends UntypedActor{
+public class Summator extends UntypedActor {
+
+	private final ActorRef master;
+	private int NUMBER_OF_SLAVES = 2;
+	
+	private final List<Integer> state = new ArrayList<>(NUMBER_OF_SLAVES);
+	private ActorRef slave1;
+	private ActorRef slave2;
+
+	public Summator(ActorRef master) {
+		this.master = master;
+	}
 
 	@Override
 	public void onReceive(Object msg) throws Throwable {
-		
+
+		if (msg instanceof int[]) {//message came from the master
+
+			int from = ((int[]) msg)[0];
+			int to = ((int[]) msg)[1];
+
+			if (to - from > 3) {
+				//structural recursion, 
+				//too much work, give the work out to spawned slaves
+				(slave1 = getContext().actorOf(
+						Props.create(Summator.class, getSelf()))).tell(
+						new int[] { from, (from + to) >>> 1 }, getSelf());
+				
+				(slave2 = getContext().actorOf(
+						Props.create(Summator.class, getSelf()))).tell(
+						new int[] { ((from + to) >>> 1)+1,to }, getSelf());
+			}else {//can do work myself
+				master.tell(calc(from,to), getSelf());
+			}
+		}else if(msg instanceof Integer) {//result comes fromo slaves through here
+			
+			state.add((Integer) msg);
+			if(state.size() == NUMBER_OF_SLAVES) {
+				
+				master.tell(state.get(0)+state.get(1), getSelf());
+				
+				//actor finished theri tasks, kill 'em now
+				slave1.tell(PoisonPill.getInstance(), getSelf());
+				slave2.tell(PoisonPill.getInstance(), getSelf());
+			}
+		}
+
 	}
 
-	
-	
+	private int calc(int from, int to) {
+
+		int result = 0;
+
+		for (int k = from; k <= to; k++) {
+
+			result += k;
+		}
+
+		return result;
+	}
+
 }
