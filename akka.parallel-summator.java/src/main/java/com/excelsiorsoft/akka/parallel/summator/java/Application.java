cdfc7a717ff8f58c97eb635578ea0894e46881d8
sleyzerzon <senya22@yahoo.com>
2016-09-12 16:38:37 -0400

    parallel-summator

---
+++ akka.parallel-summator.java/src/main/java/com/excelsiorsoft/akka/parallel/summator/java/Application.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.excelsiorsoft.akka.parallel.summator.java;
+
+import java.util.Scanner;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
+/**
+ * @author Simeon
+ *
+ */
+public class Application {
+	
+	public static void main(String[] args) {
+		ActorSystem system = ActorSystem.create("demo");
+		ActorRef worker = system.actorOf(Props.create(WorkerUpcaser.class),"worker");
+		ActorRef callback = system.actorOf(Props.create(Listener.class),"listener");
+		
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()) {
+			String line = sc.nextLine();
+			if("exit".equals(line)) {
+				system.shutdown();
+				return;
+			}
+			worker.tell(line, callback);
+			//worker.tell(line, ActorRef.noSender()); //"null object" pattern
+		}
+		
+	}
+
+}
---
+++ akka.parallel-summator.java/src/main/java/com/excelsiorsoft/akka/parallel/summator/java/WorkerUpcaser.java
@@ -0,0 +1,27 @@
+package com.excelsiorsoft.akka.parallel.summator.java;
+
+import akka.actor.ActorRef;
+import akka.actor.UntypedActor;
+
+public class WorkerUpcaser extends UntypedActor{
+
+	@Override
+	public void onReceive(Object msg) throws Throwable {
+
+		if(msg instanceof String) {
+			String response = ((String)msg).toUpperCase();
+			ActorRef sender = getSender();
+			sender.tell(response, getSelf());
+			
+			sender.tell("X", getSelf());
+			sender.tell("Y", getSelf());
+		
+		}else {
+			unhandled(msg);
+		}
+		
+	}
+
+	
+	
+}
