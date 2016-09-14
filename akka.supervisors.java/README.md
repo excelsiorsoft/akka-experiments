bccb53c44b7262943bfea5203e17b278961f21d9
sleyzerzon <senya22@yahoo.com>
2016-09-14 08:56:01 -0400

    supervisors

---
+++ akka.supervisors.java/README.md
@@ -0,0 +1,10 @@
+# akka.parallel-hashmap.java
+
+This is an implementation of a distributed HashMap on top of Akka
+
+- To start - execute Application.main() in IDE (or console).
+
+[get/result, key-A, A]
+[get/result, key-B, B]
+[get/result, key-C, C]
+       
---
+++ akka.supervisors.java/src/main/java/com/excelsiorsoft/akka/parallel/hashmap/java/Application.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.excelsiorsoft.akka.parallel.hashmap.java;
+
+import static akka.actor.ActorRef.noSender;
+import static com.excelsiorsoft.akka.parallel.hashmap.java.Container.*;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
+/**
+ * @author Simeon
+ *
+ */
+public class Application {
+	
+	public static void main(String[] args) throws Throwable {
+		
+		ActorSystem system = ActorSystem.create("demo");
+		ActorRef container = system.actorOf(Props.create(Container.class), "container");
+		ActorRef listener = system.actorOf(Props.create(Listener.class), "listener");
+		
+		container.tell(msg(PUT, "key-A", "A"),  noSender());
+		container.tell(msg(PUT, "key-B", "B"),  noSender());
+		container.tell(msg(PUT, "key-C", "C"),  noSender());
+		
+		container.tell(msg(REMOVE, "keyB"), noSender());
+		
+		container.tell(msg(GET, "key-A"), listener);
+		container.tell(msg(GET, "key-B"), listener);
+		container.tell(msg(GET, "key-C"), listener);
+		
+		System.in.read();
+		system.shutdown();
+	}
+
+	public static Object[] msg(Object... elements) {
+		
+		return elements;
+	}
+
+	
+
+}
