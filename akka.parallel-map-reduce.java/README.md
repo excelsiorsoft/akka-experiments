5b5dc41028f022c2e21e9d6d9a2ec940bb79d6ed
sleyzerzon <senya22@yahoo.com>
2016-09-13 21:17:00 -0400

    map-reduce

---
+++ akka.parallel-map-reduce.java/README.md
@@ -0,0 +1,8 @@
+# akka.parallel-map-reduce.java
+
+This is an implementation of a parallel map/reduce on top of Akka
+
+- To start - execute Application.main() in IDE (or console).
+
+
+       
---
+++ akka.parallel-map-reduce.java/src/main/java/com/excelsiorsoft/akka/parallel/mapreduce/java/Container.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package com.excelsiorsoft.akka.parallel.mapreduce.java;
+
+import static akka.actor.Props.create;
+import static com.excelsiorsoft.akka.parallel.mapreduce.java.Utils.printMsg;
+import akka.actor.ActorRef;
+import akka.actor.UntypedActor;
+
+/**
+ * @author Simeon
+ *
+ */
+public class Container extends UntypedActor {
+	
+	public static final String GET_RESULT = "get/result";
+	public static final String GET = "get";
+	public static final String REMOVE = "remove";
+	public static final String PUT = "put";
+	
+	private static final int NUM_OF_BUCKETS = 16;
+	ActorRef[] buckets = new ActorRef[NUM_OF_BUCKETS];
+	
+	public Container() {
+		
+		for(int k = 0; k < buckets.length; k++) {
+			buckets[k] = getContext().actorOf(create(Bucket.class), "bucket-"+k);
+		}
+	}
+	
+	
+	@Override
+	public void onReceive(Object msg) throws Throwable {
+
+	Object[] msgArr = (Object[])msg; //this is an agreed upon request shape
+		
+		String command = (String)msgArr[0];
+		String key = (String)msgArr[1];
+		
+		//protocol is: {command, key, ...}
+		
+		switch(command) {
+									//just proxy to the bucket
+		case PUT: 					//{"put", key, value} -> {"put", key, value}
+
+		case REMOVE: 				//{"remove", key} -> {"remove", key}
+			
+			   												//getSelf() - not strictly necessary as no response is expected
+			buckets[key.hashCode() % NUM_OF_BUCKETS].tell(msg, getSelf()); 
+			break;
+
+		
+		case GET: 				//{"get", key} -> {"get", key, originalSender}
+			
+			buckets[key.hashCode() % NUM_OF_BUCKETS].tell(new Object[]{GET, key, getSender()}, getSelf());
+			break;
+
+		
+		case GET_RESULT: 			//{"get/result", key, value, originalSender} -> {"get/result", key, value}
+			
+			String value = (String)msgArr[2];
+
+			ActorRef originalSender = (ActorRef)msgArr[3]; //listener
+			Object[] responseGet = {GET_RESULT, key, value};
+			originalSender.tell(responseGet, getSelf());
+			break;
+		}
+		
+	}
+
+}
